e53b53c370db56e004f4c33e7edda27ad374100e
Gumxr <algu23ac@student.cbs.dk>
2024-10-23 19:24:53 +0200

    Pinger

--- app.js
+++ app.js
@@ -10,7 +10,9 @@ app.use(express.json()); // Middleware for at parse JSON-body
 // Serve the HTML file
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public/index.html'));
-});// API Endpoint til at hente vejrdata fra Open-Meteo
+});
+
+// API Endpoint til at hente vejrdata fra Open-Meteo
 app.get('/api/weather', async (req, res) => {
     const latitude = 55.6761; // Breddegrad for København
     const longitude = 12.5683; // Længdegrad for København
@@ -24,6 +26,18 @@ app.get('/api/weather', async (req, res) => {
         res.status(500).json({ message: 'Kunne ikke hente vejrdata' });
     }
 });
+
+
+app.get('/api/ping', (req, res) => {
+    const serverReceivedTime = Date.now();
+    console.log(`Ping received at: ${serverReceivedTime} ms`);
+
+    res.sendStatus(200);
+
+    const serverResponseTime = Date.now();
+    console.log(`Response sent at: ${serverResponseTime} ms`);
+});
+
 
 // Start serveren
 const PORT = process.env.PORT || 3000;
